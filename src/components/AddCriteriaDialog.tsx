d411694b36eaace191d1c8efeb3c56291aad04b6
Matthew James <matthew.d.james87@gmail.com>
2017-06-02 21:26:20 +0100

    Dialog can create new criteria

--- src/components/AddCriteriaDialog.tsx
+++ src/components/AddCriteriaDialog.tsx
@@ -2,11 +2,18 @@ import * as React from "react";
 import * as ReactModal from "react-modal";
 import { connect } from "react-redux";
 
+import { addCriterion } from "actions/criteria";
 import { closeModal, openModal } from "actions/modals";
 import { IState } from "models";
 import { IAddCriteriaDialogProps } from "models/props";
+import { Priority } from "types";
 
 class AddCriteriaDialog extends React.Component<IAddCriteriaDialogProps, {}> {
+
+    private criterionKeyInput: HTMLInputElement;
+    private criterionOrderInput: HTMLInputElement;
+    private criterionPriorityInput: HTMLInputElement;
+
     public render(): JSX.Element {
 
         const selectedSubject = this.props.subjects.filter(subject =>
@@ -43,10 +50,24 @@ class AddCriteriaDialog extends React.Component<IAddCriteriaDialogProps, {}> {
                             </button>
                     </div>
                     <div className="card-block">
+                        <div className="input-group">
+                            <input className="form-control"
+                                type="text"
+                                placeholder="Create criterion"
+                                ref={(input) =>
+                                    this.criterionKeyInput = input} />
+                            <input type="checkbox"
+                                ref={(input) =>
+                                    this.criterionOrderInput = input} />
+                            <input type="number"
+                                ref={(input) =>
+                                    this.criterionPriorityInput = input} />
+                        </div>
                         {criteriaElements}
                     </div>
                     <div className="card-footer text-right">
-                        <button className="btn btn-primary">
+                        <button className="btn btn-primary"
+                            onClick={this.handleClickCreate.bind(this)}>
                             Create
                             </button>
                         <button className="btn btn-secondary"
@@ -61,6 +82,17 @@ class AddCriteriaDialog extends React.Component<IAddCriteriaDialogProps, {}> {
 
     private handleClick() {
         this.props.dispatch(openModal("addCriterionDialog"));
+    }
+
+    private handleClickCreate() {
+        const currentSubject = this.props.subjects.filter(
+            subject => subject.name === this.props.selectedSubject.name)[0];
+        this.props.dispatch(addCriterion(currentSubject, {
+            key: this.criterionKeyInput.value,
+            order: this.criterionOrderInput.checked ? "asc" : "desc",
+            priority: parseInt(this.criterionPriorityInput.value) as Priority
+        }));
+        this.props.dispatch(closeModal());
     }
 
     private handleRequestClose() {
