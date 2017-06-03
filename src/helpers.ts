3c8953e24519ad292aebbb0dc001d6278b658b03
Matthew James <matthew.d.james87@gmail.com>
2017-06-03 16:27:05 +0100

    Added helper function to find missing keyValues

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,5 +1,14 @@
 import { ICriterion, IInstance, IKeyValue } from "models";
 
+/**
+ * Orders given instances according to the given criteria.
+ *
+ * @export
+ * @param {ICriterion[]} criteria - a set of criteria by which to order the
+ *      instances
+ * @param {IInstance[]} instances - the instances to be ordered
+ * @returns {IInstance[]} - the ordered instances
+ */
 export function orderInstances(criteria: ICriterion[],
     instances: IInstance[]): IInstance[] {
 
@@ -22,8 +31,30 @@ export function orderInstances(criteria: ICriterion[],
     });
 };
 
+/**
+ * Calculates the value of a matching criterion and keyValue.
+ *
+ * @param {ICriterion} criterion - a criterion with a key equal to keyValue.key
+ * @param {IKeyValue} keyValue - a keyValue with a key equal to criterion.key
+ * @returns {number} - the value of the keyValue according to the criterion
+ */
 function calculateValue(criterion: ICriterion, keyValue: IKeyValue): number {
     const order = criterion.order === "asc" ? 1 : -1;
 
     return keyValue.value * order * Math.log(criterion.priority);
+}
+
+/**
+ * Returns whether an instance's keyValues that exist in criteria are missing.
+ *
+ * @param {ICriterion[]} criteria - the criteria to check keyValues against
+ * @param {IKeyValue[]} keyValues - the list of keyValues to check
+ * @returns {boolean} - true if a keyValue is missing that exists in criteria
+ */
+export function isMissingKeyValue(criteria: ICriterion[],
+    keyValues: IKeyValue[]): boolean {
+
+    return criteria.length === keyValues.length && criteria.every(criterion =>
+        keyValues.filter(keyValue =>
+            criterion.key === keyValue.key).length === 1);
 }
