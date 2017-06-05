ff9c0235c82c526de11d151199a5d34c67a59777
Matthew James <matthew.d.james87@gmail.com>
2017-06-05 21:21:10 +0100

    User's edits persist in model

--- src/components/dialogs/EditInstanceDialog.tsx
+++ src/components/dialogs/EditInstanceDialog.tsx
@@ -2,38 +2,49 @@ import * as React from "react";
 import * as ReactModal from "react-modal";
 import { connect } from "react-redux";
 
-import { setSelectedInstanceName } from "actions/instances";
+import {
+    addInstance,
+    deleteInstance,
+    setSelectedInstanceName
+} from "actions/instances";
 import { closeModal } from "actions/modals";
-import { IState } from "models";
+import { IKeyValue, IState } from "models";
 import { IEditInstanceDialogProps } from "models/props";
 
 class EditInstanceDialog extends React.Component<IEditInstanceDialogProps, {}> {
 
     private instanceNameInput: HTMLInputElement;
-    private keyValueInputs: HTMLInputElement[];
+    private criteriaInputs: HTMLInputElement[];
 
     constructor(props: IEditInstanceDialogProps) {
         super(props);
 
-        this.keyValueInputs = new Array<HTMLInputElement>();
+        this.criteriaInputs = new Array<HTMLInputElement>();
     }
 
     public render(): JSX.Element {
         const selectedSubject = this.props.subjects.filter(
             subject => subject.name === this.props.selectedSubjectName)[0];
+
         const selectedInstance = selectedSubject.instances.filter(
             instance => instance.name === this.props.selectedInstanceName)[0];
-        const keyValueElements = selectedInstance ? selectedInstance.values.map(
-            keyValue => <div className="col-12">
+
+        const criteriaElements = selectedSubject.criteria.map(criterion => {
+            const relevantKeyValue = selectedInstance &&
+                selectedInstance.values.filter(
+                    keyValue => keyValue.key === criterion.key)[0];
+
+            return <div className="col-12">
                 <div className="input-group">
-                    <span className="input-group-addon">{keyValue.key}</span>
+                    <span className="input-group-addon">{criterion.key}</span>
                     <input type="number"
-                        id={keyValue.key}
+                        id={criterion.key}
+                        value={relevantKeyValue && relevantKeyValue.value}
                         className="form-control"
-                        ref={(input) => this.keyValueInputs.push(input)} />
+                        ref={(input) => this.criteriaInputs.push(input)} />
                 </div>
-            </div>
-        ) : null;
+            </div>;
+        });
 
         return <ReactModal isOpen={this.props.isShowingModal}
             contentLabel="EditCriteraDialog"
@@ -50,10 +61,14 @@ class EditInstanceDialog extends React.Component<IEditInstanceDialogProps, {}> {
                         value={selectedInstance.name}
                         ref={(input) =>
                             this.instanceNameInput = input} />}
-                    {keyValueElements}
+                    {criteriaElements}
                 </div>
                 <div className="card-footer text-right">
                     <button className="btn btn-primary"
+                        onClick={this.handleClickEdit.bind(this)}>
+                        Edit
+                            </button>
+                    <button className="btn btn-secondary"
                         onClick={this.handleRequestClose.bind(this)}>
                         Close
                             </button>
@@ -62,9 +77,29 @@ class EditInstanceDialog extends React.Component<IEditInstanceDialogProps, {}> {
         </ReactModal>;
     }
 
+    private handleClickEdit() {
+        const currentSubject = this.props.subjects.filter(
+            subject => subject.name === this.props.selectedSubjectName)[0];
+        this.props.dispatch(deleteInstance(currentSubject,
+            this.props.selectedInstanceName!));
+        this.props.dispatch(addInstance(currentSubject, {
+            name: this.instanceNameInput.value,
+            values: this.parseInputs()
+        }));
+
+        this.handleRequestClose();
+    }
+
     private handleRequestClose() {
         this.props.dispatch(setSelectedInstanceName(null));
         this.props.dispatch(closeModal());
+    }
+
+    private parseInputs(): IKeyValue[] {
+        return this.criteriaInputs.filter(input => input).map(input => ({
+            key: input.id,
+            value: parseInt(input.value)
+        } as IKeyValue));
     }
 };
 
