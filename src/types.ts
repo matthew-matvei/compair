51d50e318022ce7bb48868d426acaa39e2ee9455
Matthew James <matthew.d.james87@gmail.com>
2017-06-10 18:17:46 +0100

    Commented types

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,16 @@
+/**
+ * A type describing whether to arrange instances in ascending or decending
+ * order.
+ */
 export type Order = "asc" | "desc";
+
+/**
+ * A type describing the accepted values of priority for a criterion.
+ */
 export type Priority = 1 | 2 | 3 | 4 | 5;
+
+/**
+ * A type describing the possible names of modals to be shown.
+ */
 export type Modals = "addCriterionDialog" | "addInstanceDialog" |
     "editInstanceDialog" | false;
