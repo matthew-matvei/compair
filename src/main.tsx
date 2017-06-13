d25a5a2b5dee65521777b008ec845c2fd162aaae
Matthew James <matthew.d.james87@gmail.com>
2017-06-13 21:18:17 +0100

    Trying to use remote

--- src/main.tsx
+++ src/main.tsx
@@ -20,11 +20,16 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, Store } from "redux";
+const remote = require("electron").remote;
 
 import { App } from "components";
 import rootReducer from "reducers";
 
 const store: Store<{}> = createStore(rootReducer);
+
+remote.getCurrentWindow().on("close", () => {
+    console.log("window was closed!");
+});
 
 ReactDOM.render(<Provider store={store}>
     <App />
