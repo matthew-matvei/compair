0ae600692fa698672db3a2978ccd739686420c8c
Matthew James <matthew.d.james87@gmail.com>
2017-06-17 16:21:35 +0100

    Added packaging config

---
+++ package.config.js
@@ -0,0 +1,23 @@
+const packager = require("electron-packager");
+
+const ignore = (filePath) => filePath.includes(".js.map") ||
+    filePath.includes("Test");
+
+const options = {
+    dir: "app",
+    arch: "all",
+    out: "packages",
+    overwrite: "true",
+    platform: "linux,darwin",
+    ignore: ignore,
+    // mac-specific
+    appBundleId: "compair",
+    appCategoryType: "public.app-category.utilities"
+};
+
+packager(options, (error, appPaths) => {
+    if (error) {
+        console.log(error);
+    }
+    console.log(appPaths);
+});
