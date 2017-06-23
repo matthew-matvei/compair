7fdab639761ec1d54fbd735f72f7a38a9670c39c
Matthew James <matthew.d.james87@gmail.com>
2017-06-23 21:21:09 +0100

    Added criterion props

---
+++ scripts/models/props/blocks/ICriterionProps.ts
@@ -0,0 +1,9 @@
+import { Priority } from "types";
+
+interface ICriterionProps {
+    keyInputValue?: string;
+    orderInputChecked?: boolean;
+    priorityInputValue?: Priority;
+}
+
+export default ICriterionProps;
