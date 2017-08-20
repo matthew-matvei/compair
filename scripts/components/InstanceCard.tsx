57a2780c4d9ef8935056435789c05127498d10e6
Matthew James <matthew.d.james87@gmail.com>
2017-08-20 20:44:15 +0100

    Implemented alert chip when value missing

--- scripts/components/InstanceCard.tsx
+++ scripts/components/InstanceCard.tsx
@@ -1,16 +1,17 @@
 import { Card, CardActions, CardHeader } from "material-ui/Card";
+import Chip from "material-ui/Chip";
 import { GridTile } from "material-ui/GridList";
 import RaisedButton from "material-ui/RaisedButton";
+import { red50, red600 } from "material-ui/styles/colors";
 import * as React from "react";
 
-import { getMaxScore, getMinScore, showScore } from "helpers";
+import { getMaxScore, getMinScore, isMissingKeyValue, showScore } from "helpers";
 import { IInstanceCardProps } from "models/props";
 
 /**
  * A card representing an instance of a subject.
  */
-export default class InstanceCard extends
-    React.Component<IInstanceCardProps, {}> {
+export default class InstanceCard extends React.Component<IInstanceCardProps, {}> {
 
     /**
      * Defines the rendering of this component.
@@ -20,21 +21,25 @@ export default class InstanceCard extends
     public render(): JSX.Element {
         const { currentSubject, instance } = this.props;
 
-        /*const classes = currentSubject && instance ?
-            classNames({
-                "card-inverse card-danger": isMissingKeyValue(
-                    currentSubject.criteria, instance.values)
-            }) : "";*/
-
         const minScore = currentSubject && getMinScore(currentSubject.instances) || 0;
         const maxScore = currentSubject && getMaxScore(currentSubject.instances) || 0;
 
+        const wrapperStyle: React.CSSProperties = {
+            textAlign: "right",
+            display: "flex",
+            justifyContent: "flex-end"
+        };
+
         const cardContent = instance && currentSubject ?
-            <Card className="mb-4">
+            <Card>
                 <CardHeader
                     title={instance.name}
                     subtitle={showScore(instance.score, minScore, maxScore)} />
-                <CardActions style={{ textAlign: "right" }}>
+                <CardActions style={wrapperStyle}>
+                    {isMissingKeyValue(currentSubject.criteria, instance.values) &&
+                        <Chip backgroundColor={red600} labelColor={red50}>
+                            Missing value
+                        </Chip>}
                     <RaisedButton
                         label="Edit"
                         onClick={this.handleClick.bind(this)} />
@@ -45,9 +50,10 @@ export default class InstanceCard extends
                 </CardActions>
             </Card> : <Card className="mb-4">
                 <CardHeader subtitle="New instance" />
-                <CardActions >
+                <CardActions style={{ textAlign: "right" }}>
                     <RaisedButton
                         label="Create"
+                        primary
                         onClick={this.handleClickOpenDialog.bind(this)} />
                 </CardActions>
             </Card>;
